be90828b39c1b1a3b975e5a208fee198b23ddc39
habibollah <aliahmadmohammadi412@gmail.com>
2024-06-30 20:16:49 +0330

    1403/04/10

---
+++ light-skin/fetchUserSocialinfoData.php
@@ -0,0 +1,23 @@
+<?php session_start();?>
+<?php
+//Include your database connection file
+include 'dataBase.php';
+
+//Query to fetch user data  
+$query = "SELECT * FROM usersocialinfo WHERE `id` ='" . $_SESSION['id'] . "';" ;
+$result = mysqli_query($conn, $query);
+
+if(mysqli_num_rows($result) > 0) {
+  //Fetch and return the user data as JSON
+  $userData = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  
+  //  echo $userData;
+  //this echo is needed in other part of index page do not delete it
+  echo json_encode($userData);
+} else {
+  echo 'کاربری پیدا نشد';
+}
+
+//Close the database connection
+mysqli_close($conn);
+?>
